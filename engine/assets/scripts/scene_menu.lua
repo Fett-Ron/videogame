7b4b7ee4dcf5d7620f2cd72d47dd12c5ccdb2b49
Fett-Ron <marcelo.picado@ucr.ac.cr>
2026-05-14 03:21:57 -0600

    nuevo menu

--- engine/assets/scripts/scene_menu.lua
+++ engine/assets/scripts/scene_menu.lua
@@ -31,7 +31,7 @@ scene = {
             components = {
                 clickable = {},
                 text = {
-                    text = "Galaxian",
+                    text = "zombiE gunN",
                     fontId = "press_start_32",
                     r = 150,
                     g = 0,
@@ -52,7 +52,7 @@ scene = {
                     path = "./assets/scripts/menu_button_01.lua",
                 },
                 text = {
-                    text = "level 01",
+                    text = "Jugar",
                     fontId = "press_start_24",
                     r = 150,
                     g = 150,
@@ -61,6 +61,59 @@ scene = {
                 },
                 transform = {
                     position = {x = 50.0, y = 150.0},
+                    scale  = {x = 1.0, y = 1.0},
+                    rotation = 0.0,
+                }
+            }
+        },
+        {
+            components = {
+                clickable = {},
+                text = {
+                    text = "muevase con WASD",
+                    fontId = "press_start_24",
+                    r = 200,
+                    g = 150,
+                    b = 0,
+                    a = 255
+                },
+                transform = {
+                    position = {x = 50.0, y = 300.0},
+                    scale  = {x = 1.0, y = 1.0},
+                    rotation = 0.0,
+                }
+            }
+        },
+        {
+            components = {
+                clickable = {},
+                text = {
+                    text = "dispare con J",
+                    fontId = "press_start_24",
+                    r = 200,
+                    g = 150,
+                    b = 0,
+                    a = 255
+                },
+                transform = {
+                    position = {x = 50.0, y = 350.0},
+                    scale  = {x = 1.0, y = 1.0},
+                    rotation = 0.0,
+                }
+            }
+        },{
+            components = {
+                clickable = {},
+                text = {
+                    text = "cierre el juego con ESC",
+                    fontId = "press_start_24",
+                    r = 200,
+                    g = 150,
+                    b = 0,
+                    a = 255
+                },
+                transform = {
+                    position = {x = 50.0, y = 400.0},
                     scale  = {x = 1.0, y = 1.0},
                     rotation = 0.0,
                 }
